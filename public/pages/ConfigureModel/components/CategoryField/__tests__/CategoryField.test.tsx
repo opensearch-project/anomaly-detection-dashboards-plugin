0119573062036dd4f0c68efc67045ab82a7705ba
Tyler Ohlsen <ohltyler@amazon.com>
2021-06-07 16:03:55 -0700

    Update and add UT

--- public/pages/ConfigureModel/components/CategoryField/__tests__/CategoryField.test.tsx
+++ public/pages/ConfigureModel/components/CategoryField/__tests__/CategoryField.test.tsx
@@ -25,7 +25,7 @@
  */
 
 import React, { Fragment } from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent, getByRole } from '@testing-library/react';
 import { Form, Formik } from 'formik';
 import { CategoryField } from '../CategoryField';
 
@@ -66,7 +66,13 @@ describe('<CategoryField /> spec', () => {
     expect(queryByText('Enable categorical field')).not.toBeNull();
   });
   test('renders the component when enabled', () => {
-    const { container, queryByText, queryByTestId } = render(
+    const {
+      container,
+      queryByText,
+      queryByTestId,
+      getByTestId,
+      getByText,
+    } = render(
       <Fragment>
         <Formik
           initialValues={{
@@ -78,7 +84,7 @@ describe('<CategoryField /> spec', () => {
             <Form>
               <CategoryField
                 isHCDetector={true}
-                categoryFieldOptions={['option 1', 'option 2']}
+                categoryFieldOptions={['a', 'b']}
                 setIsHCDetector={(isHCDetector: boolean) => {
                   return;
                 }}
@@ -99,6 +105,9 @@ describe('<CategoryField /> spec', () => {
     expect(queryByTestId('noCategoryFieldsCallout')).toBeNull();
     expect(queryByTestId('categoryFieldComboBox')).not.toBeNull();
     expect(queryByText('Enable categorical field')).not.toBeNull();
+    fireEvent.click(getByTestId('comboBoxToggleListButton'));
+    getByText('a');
+    getByText('b');
   });
   test('shows callout when there are no available category fields', () => {
     const { container, queryByText, queryByTestId } = render(
@@ -168,5 +177,49 @@ describe('<CategoryField /> spec', () => {
     expect(container).toMatchSnapshot();
     expect(queryByTestId('noCategoryFieldsCallout')).toBeNull();
     expect(queryByText('Enable categorical field')).not.toBeNull();
+  });
+  test(`limits selection to a maximum of 2 entities`, () => {
+    const { getAllByRole, getByTestId, queryByText } = render(
+      <Fragment>
+        <Formik
+          initialValues={{
+            categoryField: [],
+          }}
+          onSubmit={() => {}}
+        >
+          <Fragment>
+            <Form>
+              <CategoryField
+                isHCDetector={true}
+                categoryFieldOptions={['a', 'b', 'c']}
+                setIsHCDetector={(isHCDetector: boolean) => {
+                  return;
+                }}
+                isLoading={false}
+                originalShingleSize={1}
+              />
+            </Form>
+          </Fragment>
+        </Formik>
+      </Fragment>
+    );
+    // open combo box
+    fireEvent.click(getByTestId('comboBoxToggleListButton'));
+    expect(queryByText('a')).not.toBeNull();
+    expect(queryByText('b')).not.toBeNull();
+    expect(queryByText('c')).not.toBeNull();
+
+    // select top 3 options (a,b,c)
+    fireEvent.click(getAllByRole('option')[0]);
+    fireEvent.click(getAllByRole('option')[0]);
+    fireEvent.click(getAllByRole('option')[0]);
+
+    // close combo box
+    fireEvent.click(getByTestId('comboBoxToggleListButton'));
+
+    // the last selection (c) is still not selected
+    expect(queryByText('a')).not.toBeNull();
+    expect(queryByText('b')).not.toBeNull();
+    expect(queryByText('c')).toBeNull();
   });
 });
