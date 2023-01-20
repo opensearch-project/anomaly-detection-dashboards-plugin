b3185197beade9c84f403a3d9f9d37e678a03332
Jackie Han <jkhanjob@gmail.com>
2023-01-20 09:37:35 -0800

    Correct flyout and add partial create detector page
    Signed-off-by: Jackie Han <jkhanjob@gmail.com>

--- public/utils/contextMenu/action.tsx
+++ public/utils/contextMenu/action.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { i18n } from '@osd/i18n';
 import { EuiIconType } from '@elastic/eui/src/components/icon/icon';
+import { toMountPoint } from '../../../../../src/plugins/opensearch_dashboards_react/public';
 import { Action } from '../../../../../src/plugins/ui_actions/public';
-import { createADAction } from 'public/action/ad_dashboard_action';
+import { createADAction } from '../../action/ad_dashboard_action';
+import FormikWrapper from '../../components/FeatureAnywhereContextMenu/FormikWrapper';
+import CreateAnomalyDetector from '../../components/FeatureAnywhereContextMenu/CreateAnomalyDetector';
 
 // This is used to create all actions in the same context menu
 const grouping: Action['grouping'] = [
@@ -28,17 +31,21 @@ export const getActions = ({ core }) =>
       icon: 'plusInCircle' as EuiIconType,
       order: 100,
       onClick: async ({ embeddable }) => {
-        console.log("create ad");
-
-        // openFlyout(
-        //   toMountPoint(
-        //     <FormikWrapper {...{ getFormikOptions }}>
-        //       < {...{ embeddable }} />
-        //     </FormikWrapper>
-        //   ),
-        //   { size: 'l' }
-        // );
-
+        const services = await core.getStartServices();
+        const openFlyout = services[0].overlays.openFlyout;
+        // const getFormikOptions = () => ({
+        //   initialValues: getInitialValues(),
+        //   onSubmit: (values) => {
+        //     console.log('Submitting createAlertingMonitor');
+        //     console.log(values);
+        //   },
+        // });
+        openFlyout(
+          toMountPoint(
+            <CreateAnomalyDetector {...{ embeddable }} />
+          ),
+          { size: 'l' }
+        );
       },
     },
     {
@@ -68,4 +75,4 @@ export const getActions = ({ core }) =>
       },
     },
   ]
-  //.map((options) => createADAction({ ...options, grouping }));
+  .map((options) => createADAction({ ...options, grouping }));
