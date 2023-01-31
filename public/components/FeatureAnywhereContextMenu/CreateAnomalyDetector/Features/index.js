2adaaf89633737208c52d467c84e92653adb22af
Jackie Han <jkhanjob@gmail.com>
2023-01-30 22:55:41 -0800

    Add Detector Detail and Feature Accordion
    Signed-off-by: Jackie Han <jkhanjob@gmail.com>

---
+++ public/components/FeatureAnywhereContextMenu/CreateAnomalyDetector/Features/index.js
@@ -0,0 +1,89 @@
+import React, { useState, useEffect, useCallback } from 'react';
+import {
+  EuiHorizontalRule,
+  EuiTextColor,
+  EuiPanel,
+  EuiIcon,
+  EuiAccordion,
+  EuiFormRow,
+  EuiFieldText,
+  EuiSelect,
+  EuiFlexItem,
+  EuiFlexGroup
+} from '@elastic/eui';
+
+const Features = () => {
+  const aggMethodOptions = [
+    { value: 'avg', text: 'AVG' },
+    { value: 'sum', text: 'SUM' },
+  ];
+  const [aggMethodValue, setAggMethodValue] = useState(aggMethodOptions[0].value);
+  const aggMethodOnChange = e => {
+    setAggMethodValue(e.target.value);
+  };
+
+  return (
+    <>
+      <EuiPanel>
+        <EuiFormRow label='feature_name_1'>
+          <EuiFlexGroup alignItems={'flexStart'} gutterSize={'m'}>
+            <EuiFlexItem grow={1}>
+              <small>
+                Find anomalies based on
+              </small>
+              
+              <EuiFieldText
+                value='field value'
+              />
+            </EuiFlexItem>
+
+            <EuiFlexItem grow={1}>
+              <small>
+                Aggregation method
+              </small>
+              <EuiSelect
+                id="aggreationMethod"
+                options={aggMethodOptions}
+                value={aggMethodValue}
+                onChange={e => aggMethodOnChange(e)}
+              />
+            </EuiFlexItem>
+          </EuiFlexGroup>
+        </EuiFormRow>
+        <EuiHorizontalRule margin="s" />
+        <EuiFormRow label='feature_name_2'>
+          <EuiFlexGroup alignItems={'flexStart'} gutterSize={'m'}>
+            <EuiFlexItem grow={1}>
+              <small>
+                Find anomalies based on
+              </small>
+              
+              <EuiFieldText
+                value='field value'
+              />
+            </EuiFlexItem>
+
+            <EuiFlexItem grow={1}>
+              <small>
+                Aggregation method
+              </small>
+              <EuiSelect
+                id="aggreationMethod"
+                options={aggMethodOptions}
+                value={aggMethodValue}
+                onChange={e => aggMethodOnChange(e)}
+              />
+            </EuiFlexItem>
+          </EuiFlexGroup>
+        </EuiFormRow>
+        {/* <EuiFormRow>
+          <EuiIcon type="alert" />
+            <p><EuiTextColor color="danger">Selected aggration method is incompatible</EuiTextColor></p>
+        </EuiFormRow> */}
+      </EuiPanel>
+     
+    </>
+  );
+}
+
+export default Features;
--- public/components/FeatureAnywhereContextMenu/CreateAnomalyDetector/index.js
+++ public/components/FeatureAnywhereContextMenu/CreateAnomalyDetector/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   EuiText,
   EuiHorizontalRule,
@@ -8,9 +8,12 @@ import {
   EuiFlyoutBody,
   EuiTitle,
   EuiAccordion,
+  EuiSpacer
 } from '@elastic/eui';
 import './styles.scss';
 import { EmbeddablePanel } from '../../../../../../src/plugins/embeddable/public';
+import DetectorDetails from './DetectorDetails';
+import Features from './Features';
 
 const accordions = ['detectorDetails', 'features', 'categoricalFields', 'shingleSize', 'alerts', 'triggers'].reduce(
   (acc, cur) => ({ ...acc, [cur]: cur }),
@@ -19,6 +22,41 @@ const accordions = ['detectorDetails', 'features', 'categoricalFields', 'shingle
 
 function CreateAnomalyDetector({ embeddable }) {
   const [accordionOpen, setAccordionOpen] = useState(accordions.triggers);
+
+  // useEffect(() => {
+  //   async function createVisEmbeddable() {
+  //     try {
+  //       const getFactory = getEmbeddable().getEmbeddableFactory('visualization');
+
+  //       // fetching the current context from the data plugin
+  //       const contextInput = {
+  //         filters: getQueryService().filterManager.getFilters(),
+  //         query: getQueryService().queryString.getQuery(),
+  //         timeRange: getQueryService().timefilter.timefilter.getTime(),
+  //       };
+
+  //       const embeddable = (await getFactory?.createFromSavedObject(
+  //         props.savedObjectId,
+  //         contextInput
+  //       )) as IEmbeddable<EmbeddableInput, EmbeddableOutput> | ErrorEmbeddable;
+
+  //       // updating the input so we don't auto-refresh
+  //       embeddable.updateInput({
+  //         // @ts-ignore
+  //         refreshConfig: {
+  //           value: 0,
+  //           pause: true,
+  //         },
+  //       });
+
+  //       setEmbeddableObj(embeddable);
+  //     } catch (err) {
+  //       console.log(err);
+  //     }
+  //   }
+  //   createVisEmbeddable();
+  //   // TODO: add more if needed
+  // }, [props.savedObjectId]);
 
   return (
     <div className="create-anomaly-detector">
@@ -51,16 +89,9 @@ function CreateAnomalyDetector({ embeddable }) {
               buttonContent={
                 <EuiText>
                   <h6>Detector Details</h6>
-                  {/* {accordionOpen !== accordions.detectorDetails && (
-                    <>
-                      <EuiText size="s">{name.value}</EuiText>
-                      <EuiText size="xs" color="subdued">
-                        {anomalyDetectorText}
-                      </EuiText>
-                    </>
-                  )} */}
                 </EuiText>
               }
+              initialIsOpen={true}
               forceState={accordionOpen === accordions.detectorDetails ? 'open' : 'closed'}
               onToggle={() =>
                 setAccordionOpen(
@@ -68,8 +99,45 @@ function CreateAnomalyDetector({ embeddable }) {
                 )
               }
             >
+              <EuiSpacer />
+              <DetectorDetails />
             </EuiAccordion>
-
+            <EuiHorizontalRule margin="s" />
+            <EuiAccordion
+                id={accordions.features}
+                buttonContent={
+                  <EuiText>
+                    <h6>Features</h6>
+                  </EuiText>
+                }
+                initialIsOpen={true}
+                // forceState={accordionOpen === accordions.detectorDetails ? 'open' : 'closed'}
+                // onToggle={() =>
+                //   setAccordionOpen(
+                //     accordionOpen !== accordions.detectorDetails && accordions.detectorDetails
+                //   )
+                // }
+              >
+                <Features />
+            </EuiAccordion>
+            <EuiHorizontalRule margin="s" />
+            <EuiAccordion
+                id={accordions.shingleSize}
+                buttonContent={
+                  <EuiText>
+                    <h6>Shingle Size</h6>
+                  </EuiText>
+                }
+                initialIsOpen={false}
+                // forceState={accordionOpen === accordions.detectorDetails ? 'open' : 'closed'}
+                // onToggle={() =>
+                //   setAccordionOpen(
+                //     accordionOpen !== accordions.detectorDetails && accordions.detectorDetails
+                //   )
+                // }
+              >
+                {/* <Features /> */}
+            </EuiAccordion>
           </EuiFlexItem>
         </EuiFlexGroup>
       </EuiFlyoutBody>
