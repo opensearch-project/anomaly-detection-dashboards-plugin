7e24cd978c64464c18cf6437155e2e26cd15bd3b
Amit Galitzky <amgalitz@amazon.com>
2023-05-24 15:20:29 -0700

    merge cleanup
    Signed-off-by: Amit Galitzky <amgalitz@amazon.com>

--- public/components/FeatureAnywhereContextMenu/DocumentationTitle/index.ts
+++
@@ -1,8 +0,0 @@
-/*
- * Copyright OpenSearch Contributors
- * SPDX-License-Identifier: Apache-2.0
- */
-
-import DocumentationTitle from './containers/DocumentationTitle';
-
-export default DocumentationTitle;
