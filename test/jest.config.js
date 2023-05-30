9308a4ccb120399202446982f396186ae8dcb793
Jackie Han <hnyng@amazon.com>
2023-05-30 13:54:15 -0700

    add jest transform config (#497)
    * add jest transform config
    
    Signed-off-by: Jackie Han <jkhanjob@gmail.com>
    
    * add license header
    
    Signed-off-by: Jackie Han <jkhanjob@gmail.com>
    
    * update file as .ts file
    
    Signed-off-by: Jackie Han <jkhanjob@gmail.com>
    
    * add comment on jest config file
    
    Signed-off-by: Jackie Han <jkhanjob@gmail.com>
    
    * add more comments
    
    Signed-off-by: Jackie Han <jkhanjob@gmail.com>
    
    * cleanup
    
    Signed-off-by: Jackie Han <jkhanjob@gmail.com>
    
    * update file export
    
    Signed-off-by: Jackie Han <jkhanjob@gmail.com>
    
    ---------
    
    Signed-off-by: Jackie Han <jkhanjob@gmail.com>

--- test/jest.config.js
+++ test/jest.config.js
@@ -44,4 +44,14 @@ module.exports = {
   testPathIgnorePatterns: ['<rootDir>/build/', '<rootDir>/node_modules/'],
   transformIgnorePatterns: ['<rootDir>/node_modules'],
   globalSetup: '<rootDir>/global-setup.js',
+
+  /**
+   * This configuration specifies different file extensions 
+   * and the corresponding transformers to be used
+   */
+  transform: {
+    '\\.[jt]sx?$': 'babel-jest',
+    '^.+\\.svg$': '<rootDir>/test/mocks/transformMock.ts',
+    '^.+\\.html$': '<rootDir>/test/mocks/transformMock.ts',
+  },
 };
