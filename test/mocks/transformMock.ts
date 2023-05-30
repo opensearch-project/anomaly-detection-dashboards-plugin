2b1204b9fb8aea54f5c087c4b76b1021673d17fe
Jackie Han <jkhanjob@gmail.com>
2023-05-30 10:25:13 -0700

    add more comments
    Signed-off-by: Jackie Han <jkhanjob@gmail.com>

--- test/mocks/transformMock.ts
+++ test/mocks/transformMock.ts
@@ -2,12 +2,25 @@
  * Copyright OpenSearch Contributors
  * SPDX-License-Identifier: Apache-2.0
  */
+
+/**
+ * The transform configuration in Jest allows you to 
+ * specify custom transformation logic for specific file types during testing. 
+ */
 export = {
+  /**
+   * This function is responsible for transforming the file.
+   * @returns the string module.exports = {};, which is an empty CommonJS module.
+   */
   process(): { code: string } {
     return {
       code: `module.exports = {};`,
     };
   },
+  /**
+   * The cache key helps Jest determine if a file needs to be retransformed or if it can use the cached transformation result. 
+   * @returns returns a unique string that serves as a cache key for the transformation. 
+   */
   getCacheKey(): string {
     return 'svgTransform';
   },
