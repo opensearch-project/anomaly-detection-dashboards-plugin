efb361bb56a3fdd6cb98dc3e90e1dea79a27dd3f
Yulong Ruan <ruanyu1@gmail.com>
2023-12-28 01:13:36 +0800

    fix build error due to missing babel plugins (#661)
    Signed-off-by: Yulong Ruan <ruanyl@amazon.com>

--- babel.config.js
+++ babel.config.js
@@ -16,8 +16,8 @@ module.exports = {
     require('@babel/preset-typescript'),
   ],
   plugins: [
-    require('@babel/plugin-proposal-class-properties'),
-    require('@babel/plugin-proposal-object-rest-spread'),
+    require('@babel/plugin-transform-class-properties'),
+    require('@babel/plugin-transform-object-rest-spread'),
     ['@babel/plugin-transform-modules-commonjs', { allowTopLevelThis: true }],
     [require('@babel/plugin-transform-runtime'), { regenerator: true }],
   ],
